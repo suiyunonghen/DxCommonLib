577a918e86a524da9621d976f3984a2d742b2a21
suiyunonghen <75492895@qq.com>
2017-07-31 23:49:51 +0800

    StringList Fixed

--- StringList.go
+++ StringList.go
@@ -3,57 +3,56 @@
 GStringList类似于TStringList
 Autor: 不得闲
 QQ:75492895
- */
+*/
 package DxCommonLib
 
 import (
-	"strings"
-	"os"
 	"fmt"
+	"os"
+	"strings"
 )
 
-type(
+type (
 	IStrings interface {
-		Count()int
-		Strings(index int)string
-		SetStrings(index int,str string)
-		Text()string
+		Count() int
+		Strings(index int) string
+		SetStrings(index int, str string)
+		Text() string
 		SetText(text string)
 		LoadFromFile(fileName string)
 		SaveToFile(fileName string)
 		Add(str string)
-		Insert(Index int,str string)
+		Insert(Index int, str string)
 		Delete(index int)
 		AddStrings(strs IStrings)
 		AddSlice(strs []string)
 		Clear()
 		IndexOf(str string) int
 
-		AddPair(Name,Value string)
+		AddPair(Name, Value string)
 		IndexOfName(Name string) int
 		ValueFromIndex(index int) string
 		ValueByName(Name string) string
 		Names(Index int) string
-		AsSlice()[]string
+		AsSlice() []string
 	}
 
+	LineBreakMode byte
 
-	LineBreakMode  byte
-
-	GStringList struct{
-		strings 				[]string
-		LineBreak				LineBreakMode
-		UnknownCodeUseGbk		bool		//未知编码的时候采用GBK编码打开
+	GStringList struct {
+		strings           []string
+		LineBreak         LineBreakMode
+		UnknownCodeUseGbk bool //未知编码的时候采用GBK编码打开
 	}
 )
 
 const (
-	LBK_CRLF  LineBreakMode = iota
+	LBK_CRLF LineBreakMode = iota
 	LBK_CR
 	LBK_LF
 )
 
-func (lst *GStringList)LineBreakStr()string  {
+func (lst *GStringList) LineBreakStr() string {
 	switch lst.LineBreak {
 	case LBK_CRLF:
 		return "\r\n"
@@ -66,66 +65,65 @@ func (lst *GStringList)LineBreakStr()string  {
 	}
 }
 
-func (lst *GStringList) Count()int {
-	if lst.strings == nil{
+func (lst *GStringList) Count() int {
+	if lst.strings == nil {
 		return 0
 	}
 	return len(lst.strings)
 }
 
-
-func (lst *GStringList)Strings(index int)string{
-	if index >= 0 && index < len(lst.strings){
+func (lst *GStringList) Strings(index int) string {
+	if index >= 0 && index < len(lst.strings) {
 		return lst.strings[index]
 	}
 	return ""
 }
 
-func (lst *GStringList)SetStrings(index int,str string){
-	if lst.strings == nil{
-		lst.strings = make([]string,0,64)
+func (lst *GStringList) SetStrings(index int, str string) {
+	if lst.strings == nil {
+		lst.strings = make([]string, 0, 64)
 	}
-	if index >= 0 && index < len(lst.strings){
+	if index >= 0 && index < len(lst.strings) {
 		lst.strings[index] = str
 	}
 }
 
-func (lst *GStringList)Text()string{
-	if lst.Count()==0{
+func (lst *GStringList) Text() string {
+	if lst.Count() == 0 {
 		return ""
 	}
-	return  strings.Join(lst.strings,lst.LineBreakStr())
+	return strings.Join(lst.strings, lst.LineBreakStr())
 }
 
-func (lst *GStringList)SetText(text string){
-	lst.strings = strings.Split(text,lst.LineBreakStr())
+func (lst *GStringList) SetText(text string) {
+	lst.strings = strings.Split(text, lst.LineBreakStr())
 }
 
-func (lst *GStringList)LoadFromFile(fileName string){
-	if finfo,err := os.Stat(fileName);err == nil && !finfo.IsDir(){
-		if file,err := os.Open(fileName);err == nil{
-			databytes := make([]byte,finfo.Size())
+func (lst *GStringList) LoadFromFile(fileName string) {
+	if finfo, err := os.Stat(fileName); err == nil && !finfo.IsDir() {
+		if file, err := os.Open(fileName); err == nil {
+			databytes := make([]byte, finfo.Size())
 			file.Read(databytes)
 			isUtf8 := databytes[0] == 0xEF && databytes[1] == 0xBB && databytes[2] == 0xBF
-			if isUtf8{
+			if isUtf8 {
 				databytes = databytes[3:]
 			}
 			file.Close()
-			if !isUtf8 && lst.UnknownCodeUseGbk{
-				if tmpbytes,err := GBK2Utf8(databytes);err == nil{
+			if !isUtf8 && lst.UnknownCodeUseGbk {
+				if tmpbytes, err := GBK2Utf8(databytes); err == nil {
 					databytes = tmpbytes
 				}
 			}
-			lst.strings = strings.Split(FastByte2String(databytes),lst.LineBreakStr())
+			lst.strings = strings.Split(FastByte2String(databytes), lst.LineBreakStr())
 		}
 	}
 }
 
-func (lst *GStringList)SaveToFile(fileName string)  {
+func (lst *GStringList) SaveToFile(fileName string) {
 	//文件要先写入UTF8的BOM
-	if file,err := os.OpenFile(fileName,os.O_CREATE | os.O_TRUNC,0644);err == nil{
-		if lst.Count() > 0{
-			file.Write([]byte{0xEF,0xBB,0xBF})
+	if file, err := os.OpenFile(fileName, os.O_CREATE|os.O_TRUNC, 0644); err == nil {
+		if lst.Count() > 0 {
+			file.Write([]byte{0xEF, 0xBB, 0xBF})
 			//写入内容
 			file.Write(FastString2Byte(lst.Text()))
 		}
@@ -133,53 +131,53 @@ func (lst *GStringList)SaveToFile(fileName string)  {
 	}
 }
 
-func (lst *GStringList)Add(str string){
-	if lst.strings == nil{
-		lst.strings = make([]string,0,64)
+func (lst *GStringList) Add(str string) {
+	if lst.strings == nil {
+		lst.strings = make([]string, 0, 64)
 	}
-	lst.strings = append(lst.strings,str)
+	lst.strings = append(lst.strings, str)
 }
 
-func (lst *GStringList)Insert(Index int,str string){
-	if lst.strings == nil{
-		lst.strings = make([]string,0,64)
+func (lst *GStringList) Insert(Index int, str string) {
+	if lst.strings == nil {
+		lst.strings = make([]string, 0, 64)
 	}
-	if Index >= 0 && Index < len(lst.strings){
-		temp := append([]string{},lst.strings[Index:]...)
-		lst.strings = append(lst.strings[0:Index],str)
-		lst.strings = append(lst.strings,temp...)
-	}else{
+	if Index >= 0 && Index < len(lst.strings) {
+		temp := append([]string{}, lst.strings[Index:]...)
+		lst.strings = append(lst.strings[0:Index], str)
+		lst.strings = append(lst.strings, temp...)
+	} else {
 		lst.Add(str)
 	}
 }
-func (lst *GStringList)Delete(index int){
-	if lst.Count() > 0 && index >= 0 && index < len(lst.strings){
-		lst.strings = append(lst.strings[0:index],lst.strings[index+1:]...)
+func (lst *GStringList) Delete(index int) {
+	if lst.Count() > 0 && index >= 0 && index < len(lst.strings) {
+		lst.strings = append(lst.strings[0:index], lst.strings[index+1:]...)
 	}
 }
 
-func (lst *GStringList)AddStrings(strs IStrings){
-	for idx := 0;idx < strs.Count();idx++{
+func (lst *GStringList) AddStrings(strs IStrings) {
+	for idx := 0; idx < strs.Count(); idx++ {
 		lst.Add(strs.Strings(idx))
 	}
 }
 
-func (lst *GStringList)AddSlice(strs []string){
-	if lst.strings != nil{
-		lst.strings = append(lst.strings,strs...)
-	}else{
+func (lst *GStringList) AddSlice(strs []string) {
+	if lst.strings != nil {
+		lst.strings = append(lst.strings, strs...)
+	} else {
 		lst.strings = strs
 	}
 }
-func (lst *GStringList)Clear(){
-	if lst.strings != nil{
+func (lst *GStringList) Clear() {
+	if lst.strings != nil {
 		lst.strings = lst.strings[:0]
 	}
 }
-func (lst *GStringList)IndexOf(str string)int{
-	if lst.Count() > 0{
-		for idx,v := range lst.strings{
-			if v == str{
+func (lst *GStringList) IndexOf(str string) int {
+	if lst.Count() > 0 {
+		for idx, v := range lst.strings {
+			if v == str {
 				return idx
 			}
 		}
@@ -187,15 +185,15 @@ func (lst *GStringList)IndexOf(str string)int{
 	return -1
 }
 
-func (lst *GStringList)AddPair(Name,Value string){
-	lst.strings = append(lst.strings,fmt.Sprintf("%s=%s",Name,Value))
+func (lst *GStringList) AddPair(Name, Value string) {
+	lst.strings = append(lst.strings, fmt.Sprintf("%s=%s", Name, Value))
 }
-func (lst *GStringList)IndexOfName(Name string)int{
-	if lst.Count() > 0{
-		for idx,v := range lst.strings{
-			if eidx := strings.IndexByte(v,'=');idx > 0{
-				bt := FastString2Byte(v)
-				if FastByte2String(bt[:eidx]) == Name {
+func (lst *GStringList) IndexOfName(Name string) int {
+	if lst.Count() > 0 {
+		for idx, v := range lst.strings {
+			if eidx := strings.IndexByte(v, '='); idx > 0 {
+				bt := []byte(v)
+				if string(bt[:eidx]) == Name {
 					return idx
 				}
 			}
@@ -204,14 +202,14 @@ func (lst *GStringList)IndexOfName(Name string)int{
 	return -1
 }
 
-func (lst *GStringList)ValueFromIndex(index int)string{
-	if lst.Count() == 0{
+func (lst *GStringList) ValueFromIndex(index int) string {
+	if lst.Count() == 0 {
 		return ""
 	}
-	if index >= 0 && index < len(lst.strings){
+	if index >= 0 && index < len(lst.strings) {
 		str := lst.strings[index]
-		if idx := strings.IndexByte(str,'=');idx > 0{
-			bt := FastString2Byte(str)
+		if idx := strings.IndexByte(str, '='); idx > 0 {
+			bt := []byte(str)
 			return FastByte2String(bt[idx+1:])
 		}
 		return ""
@@ -219,12 +217,12 @@ func (lst *GStringList)ValueFromIndex(index int)string{
 	return ""
 }
 
-func (lst *GStringList)ValueByName(Name string)string{
-	if lst.Count() > 0{
-		for idx,v := range lst.strings{
-			if eidx := strings.IndexByte(v,'=');idx > 0{
-				bt := FastString2Byte(v)
-				if FastByte2String(bt[:eidx]) == Name {
+func (lst *GStringList) ValueByName(Name string) string {
+	if lst.Count() > 0 {
+		for _, v := range lst.strings {
+			if eidx := strings.IndexByte(v, '='); eidx > 0 {
+				bt := []byte(v)
+				if string(bt[:eidx]) == Name {
 					return FastByte2String(bt[eidx+1:])
 				}
 			}
@@ -232,21 +230,20 @@ func (lst *GStringList)ValueByName(Name string)string{
 	}
 	return ""
 }
-func (lst *GStringList)Names(Index int) string{
-	if lst.Count() == 0{
+func (lst *GStringList) Names(Index int) string {
+	if lst.Count() == 0 {
 		return ""
 	}
-	if Index >= 0 && Index < len(lst.strings){
+	if Index >= 0 && Index < len(lst.strings) {
 		str := lst.strings[Index]
-		if idx := strings.IndexByte(str,'=');idx > 0{
-			bt := FastString2Byte(str)
+		if idx := strings.IndexByte(str, '='); idx > 0 {
+			bt := []byte(str)
 			return FastByte2String(bt[:idx])
 		}
 		return ""
 	}
 	return ""
 }
-func (lst *GStringList)AsSlice()[]string{
+func (lst *GStringList) AsSlice() []string {
 	return lst.strings
 }
-
--- stringlist_test.go
+++ stringlist_test.go
@@ -17,4 +17,12 @@ func TestStringList(t *testing.T) {
 	lst.LoadFromFile("d:\\t.txt")
 	fmt.Println(lst.Count())
 	fmt.Println(lst.Strings(2))
+
+	lst.Clear()
+	lst.AddPair("Name1", "不得闲")
+	lst.AddPair("Age", "20")
+	fmt.Println(lst.Text())
+	fmt.Println(lst.ValueByName("Name1"))
+	fmt.Println(lst.ValueFromIndex(1))
+	fmt.Println(lst.IndexOfName("Age"))
 }
