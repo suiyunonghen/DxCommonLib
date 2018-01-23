1a5b8bf23b934373de94d38cfc6783488ce8f3e3
suiyunonghen <75492895@qq.com>
2018-01-23 14:19:15 +0800

    Small Modify

--- CommonFunc.go
+++ CommonFunc.go
@@ -276,7 +276,7 @@ func Hex2Binary(hexStr string)[]byte  {
 	if hexStr == ""{
 		return nil
 	}
-	vhex := [71]byte{'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'A':10,'B':11,'C':12,'D':13,'E':14,'F':15}
+	vhex := ['G']byte{'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'A':10,'B':11,'C':12,'D':13,'E':14,'F':15}
 	btlen := len(hexStr) / 2
 	result := make([]byte,btlen)
 	btlen = btlen << 1
